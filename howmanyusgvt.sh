c6024d5f8d63121762a4d336445b5cc8c3e55958
meisnick <87807548+meisnick@users.noreply.github.com>
2025-01-31 18:48:33 -0600

    Create howmanyusgvt.sh

---
+++ howmanyusgvt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+read -p "How many warrior instances? " num
+cat <<EOF > warriors.yml
+version: "3.7"
+services:
+  archiveteam-watchtower:
+    container_name: archiveteam-watchtower
+    image: containrrr/watchtower
+    labels:
+      - com.centurylinklabs.watchtower.enable=true
+    volumes:
+      - '/var/run/docker.sock:/var/run/docker.sock'
+    command: '--label-enable --cleanup --interval 3600'
+    restart: unless-stopped
+EOF
+for ((i=1;i<=$num;i++)); do
+  port=$((8000 + i))
+  cat <<EOF >> warriors.yml
+  archiveteam-warrior-$i:
+    container_name: archiveteam-warrior-$i
+    image: atdr.meo.ws/archiveteam/warrior-dockerfile
+    environment:
+      - DOWNLOADER=meisnick
+      - SELECTED_PROJECT=usgovernment
+      - CONCURRENT_ITEMS=6
+    stop_signal: SIGINT
+    stop_grace_period: 5m 
+    labels:
+      - com.centurylinklabs.watchtower.enable=true
+    ports:
+      - '$port:8001'
+    restart: unless-stopped
+EOF
+done
