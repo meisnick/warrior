78ef22304595ce2fc3d9ed350efdd6db65148444
meisnick <87807548+meisnick@users.noreply.github.com>
2026-01-02 03:30:35 -0600

    Update howmany.sh

--- howmany.sh
+++ howmany.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
 read -p "How many warrior instances? " num
-
 cat <<EOF > warriors.yml
 version: "3.7"
-
 services:
-
   archiveteam-watchtower:
     container_name: archiveteam-watchtower
     image: containrrr/watchtower
@@ -14,15 +10,11 @@ services:
       - com.centurylinklabs.watchtower.enable=true
     volumes:
       - '/var/run/docker.sock:/var/run/docker.sock'
-    command: '--label-enable --cleanup --interval 3600' 
+    command: '--label-enable --cleanup --interval 3600'
     restart: unless-stopped
-
 EOF
-
 for ((i=1;i<=$num;i++)); do
-  port=$((8000 + i))
   cat <<EOF >> warriors.yml
-
   archiveteam-warrior-$i:
     container_name: archiveteam-warrior-$i
     image: atdr.meo.ws/archiveteam/warrior-dockerfile
@@ -30,13 +22,11 @@ for ((i=1;i<=$num;i++)); do
       - DOWNLOADER=meisnick
       - SELECTED_PROJECT=urlteam2
       - CONCURRENT_ITEMS=6
+      - VIEWER_PORT_DISABLED=1
     stop_signal: SIGINT
-    stop_grace_period: 5m 
+    stop_grace_period: 5m
     labels:
       - com.centurylinklabs.watchtower.enable=true
-    ports:
-      - '$port:8001'
     restart: unless-stopped
-
 EOF
 done
